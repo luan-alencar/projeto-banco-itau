16ffe1a8f519fc43e1e541358b81e735a48d62f2
Luan-Augusto <luanalencar134@gmail.com>
2020-10-12 20:17:56 -0300

    criacao de nova classe ContaCorrenteEstudante

---
+++ src/david/augusto/luan/entidades/ContaCorrenteEstudante.java
@@ -0,0 +1,21 @@
+package david.augusto.luan.entidades;
+
+public final class ContaCorrenteEstudante extends Conta {
+
+	public ContaCorrenteEstudante(int agencia, int conta, String titular, double saldo) {
+		super(agencia, conta, titular, saldo);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ContaCorrenteEstudante(int agencia, int conta, String titular) {
+		super(agencia, conta, titular);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void sacar(double montante) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
